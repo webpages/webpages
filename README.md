1b68e0ff986a784734d96ab5de1dc84f24ecda60
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-06-23 15:01:03 +0300

    Updated readme

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
 WebPages - python web framework
-===
+=====
 
 Hello.
 
 This project is based on good ideas from **Ruby on Rails** and **Django** web frameworks. Also I wish to add asynchronous code execution like in **Tornado** and **Node.js**. I tried to simplify project structure to make project as easy as it possible. I believe that you find it useful.
 
 
-#### Current tasks and bugs
+Quick start
+-----
+
+```shell
+git clone git@github.com:webpages/examples.git
+cd examples/
+pip install -r requirements.txt
+# run server on 127.0.0.1:8000
+python main.py
+```
+
+
+Current tasks and bugs
+-----
 
 Find list of tasks and bug reports in our [issues list](https://github.com/webpages/webpages/issues). Also you can add your bug report or proposition.
 
 
-#### Documentation
+Documentation
+-----
 
 You can start from [introduction](https://github.com/webpages/docs/blob/master/chapters/Intro.md) to understand project goals.
 
@@ -19,6 +33,6 @@ Read full project [documentation](https://github.com/webpages/docs). If you know
 
 
 Contact us
-----
+-----
 
 Join to us on [Facebook](https://www.facebook.com/WebPagesFramework)
--- setup.py
+++ setup.py
@@ -1,18 +1,42 @@
 """
-WebPages - python web framework
-=====
+WebPages framework
+====================
 
 This project designed for web developers who want to do more for the less time.
 To create new project with Hello World and database connection you need few minutes.
 No need to create own user registration and authentication system - save your time
-and focus on your main task - create anaming product in short time.
+and focus on your main task - create amazing product in short time.
 
 
-Development version
------
+Quick start
+-------------
 
-    https://github.com/webpages/webpages
+Clone and run example application. Don't forget to swith to your project's virtualenv.
+
+.. code:: shell
+
+   git clone git@github.com:webpages/examples.git
+   cd examples/
+   pip install -r requirements.txt
+   # run server on 127.0.0.1:8000
+   python main.py
+
+
+Development
+-------------
+
+You can install development version by clone `git repository <https://github.com/webpages/webpages>`_::
+
     git clone git@github.com:webpages/webpages.git
+
+
+Links
+-------
+
+* `project website <https://github.com/webpages/webpages>`_
+* `documentation <https://github.com/webpages/docs>`_
+* `sample applications <https://github.com/webpages/examples>`_
+
 """
 
 try:
@@ -21,7 +45,7 @@ except ImportError:
     from distutils.core import setup
 
 
-version = '1.0.0a'
+version = '1.0.0a1'
 
 
 setup(
